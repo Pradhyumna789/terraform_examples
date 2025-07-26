840d56acb3cecfa05b422fe56ae0a57bad7cfbfa
Pradhyumna789 <hariharshasripradhyumna@gmail.com>
2025-07-26 05:47:09 +0530

    count-meta-arguments

---
+++ count/variables.tf
@@ -0,0 +1,15 @@
+variable "filename" {
+    default = [
+        "cats.txt",
+        "dogs.txt",
+        "pigs.txt"
+    ]
+}
+
+variable "content" {
+    default = [
+        "I am cat",
+        "I am dog",
+        "I am pig"
+    ]
+}
